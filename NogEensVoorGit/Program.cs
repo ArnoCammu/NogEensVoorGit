fa3fcd636e2e1e6f1e8f31d0cbcdbc497c1177c0
arno cammu <arnocammu@hotmail.com>
2020-03-27 11:33:22 +0100

    Add project files.

---
+++ NogEensVoorGit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NogEensVoorGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Een project moet je eerst in de Local repository bewaren.
+            // git is gemaakt door de linux-creator
+
+            //1. Ass to source control
+
+
+        }
+    }
+}
